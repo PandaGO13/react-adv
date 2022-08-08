7dd1afb451967e1cca3ab387c09161905e66dd02
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-08-08 00:19:35 -0500

    feat: add lazy pages

---
+++ src/01-lazyload/pages/LazyPage01.tsx
@@ -0,0 +1,5 @@
+export const LazyPage01 = () => {
+  return <h1>LazyPage01</h1>;
+};
+
+export default LazyPage01;
---
+++ src/01-lazyload/pages/LazyPage02.tsx
@@ -0,0 +1,5 @@
+export const LazyPage02 = () => {
+  return <h1>LazyPage02</h1>;
+};
+
+export default LazyPage02;
